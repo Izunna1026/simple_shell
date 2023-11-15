ed4974e1f16105362c76a6b3a6ba8217f08680c3
Izunna1026 <done4emma@yahoo.com>
2023-11-15 14:50:03 +0000

    simple shell

--- show_prompt.c
+++ show_prompt.c
@@ -8,5 +8,5 @@ void show_prompt(void)
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
 		flags.interactive = 1;
 	if (flags.interactive)
-		write(STDERR_FILENO, "$", 2);
+		write(STDERR_FILENO, "izu$ ", 5);
 }
